05d7b823a832efebfa17b6b1b1fe36e1c3d38303
Shishir Ghate <shishghate@gmail.com>
2019-07-26 18:01:31 -0500

    Build cleanup and added in jewel tracker for testing

---
+++ TeamCode/src/main/java/com/edinaftc/relicrecovery/opmodes/vision/DynamicJewelVision.java
@@ -0,0 +1,55 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.edinaftc.relicrecovery.opmodes.vision;
+
+import com.edinaftc.relicrecovery.vision.DynamicJewelTracker;
+import com.edinaftc.library.vision.VuforiaCamera;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//@Disabled
+@TeleOp
+public class DynamicJewelVision extends OpMode {
+    private VuforiaCamera camera;
+    private DynamicJewelTracker jewelTracker;
+
+    @Override
+    public void init() {
+        jewelTracker = new DynamicJewelTracker();
+        camera = new VuforiaCamera();
+        camera.addTracker(jewelTracker);
+        camera.initialize();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("jewel", jewelTracker.getJewelPosition());
+    }
+}
