c262ab7730a064d3fbe63a60c305ddedcb8da400
Shishir Ghate <shishghate@gmail.com>
2019-07-30 07:26:07 -0500

    Added new empty tracker class for rr2

---
+++ TeamCode/src/main/java/com/edinaftc/relicrecovery/vision/DynamicMineralTracker.java
@@ -0,0 +1,24 @@
+package com.edinaftc.relicrecovery.vision;
+
+import com.edinaftc.library.vision.Overlay;
+import com.edinaftc.library.vision.Tracker;
+import com.edinaftc.library.vision.VisionCamera;
+
+import org.opencv.core.Mat;
+
+public class DynamicMineralTracker extends Tracker {
+    @Override
+    public void init(VisionCamera camera) {
+
+    }
+
+    @Override
+    public void processFrame(Mat frame, double timestamp) {
+
+    }
+
+    @Override
+    public void drawOverlay(Overlay overlay, int imageWidth, int imageHeight, boolean debug) {
+
+    }
+}
